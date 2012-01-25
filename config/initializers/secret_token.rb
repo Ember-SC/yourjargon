2f5d471033d3664e31fededd0ae51fc6404407a5
Richard Jones <richard.a.jones@me.com>
2012-01-24 20:31:52 -0800

    Merged conflicts

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
-Yourjargon::Application.config.secret_token = '75cbe54cef62d8ab69384e03e242d8af954455abb5cd72edd9f2082480164a67d7fd56c96169f28a84d416e6dfe2dce0925cac73268a80ce73592db81bbe4baa'
-=======
 Yourjargon::Application.config.secret_token = 'a1e5320782e96b25f900208dceee403530912002124c290de31e641c72cb3f38443147a0d2046a7184d00b13656bd41badb67dc9bc21c2751ff8c600a87beb15'
->>>>>>> develop
