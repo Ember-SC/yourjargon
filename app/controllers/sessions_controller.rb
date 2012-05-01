e7813906a93e4d89955372b1575624d9940d9365
Andrew Purdy <andrewmp1@gmail.com>
2012-05-01 16:41:13 -0700

    Adding user scaffolding & session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+   def create
+      @user = User.authenticate(params[:email], params[:password])
+
+      if @user
+      create_user_session(@user)
+      else
+        respond_with "invalid email or password"
+      end
+   end
+
+
+   def destroy
+      destroy_user_session
+   end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
-class User
+class User < ActiveRecord::Base
 
+   attr_accessor :password
+   attr_protected :password_digest
+
+   validates :email, :presence => true, :uniqueness => true
+
+
+   def password=(pass)
+     return if pass.blank?
+
+     @password = pass
+     self.password_digest = BCrypt::Password.create(pass)
+   end
+
+   def self.authenticate(email, password)
+     user = find_by_email(email)
+     user && BCrypt::Password.new(user.password_digest) == password ? user : nil
+   end
 end
