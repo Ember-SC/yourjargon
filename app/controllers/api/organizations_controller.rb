21aa1dd73581ac241d1e013a83984170720f835a
Drew Purdy <andrewmp1@gmail.com>
2013-05-21 11:50:03 -0700

    Update rails controllers to remove code responding to html and any actions that aren't part of the rest api. i.e. new, edit

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -10,75 +10,43 @@ class Api::OrganizationsController < ApplicationController
       @organizations = Organization.all
     end
 
-    respond_with @organizations
+    render json: @organizations
   end
 
-  # GET /organizations/1
-  # GET /organizations/1.json
+  # GET api/organizations/1
   def show
     @organization = Organization.find(params[:id])
 
-    respond_with @organization
+    render json: @organization
   end
 
-  # GET /organizations/new
-  # GET /organizations/new.json
-  def new
-    @organization = Organization.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @organization }
-    end
-  end
-
-  # GET /organizations/1/edit
-  def edit
-    @organization = Organization.find(params[:id])
-  end
-
-  # POST /organizations
-  # POST /organizations.json
+  # POST api/organizations
   def create
     @organization = Organization.new(params[:organization])
 
-    respond_to do |format|
-      if @organization.save
-        format.html { redirect_to @organization, notice: 'Organization was successfully created.' }
-        format.json { render json: @organization, status: :created, location: @organization }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
+    if @organization.save
+      render json: @organization, status: :created
+    else
+      render json: @organization.errors, status: :unprocessable_entity
     end
   end
 
-  # PUT /organizations/1
-  # PUT /organizations/1.json
+  # PUT api/organizations/1
   def update
     @organization = Organization.find(params[:id])
 
-    respond_to do |format|
-      if @organization.update_attributes(params[:organization])
-        format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
+    if @organization.update_attributes(params[:organization])
+      render json: @orgnanization
+    else
+      render json: @organization.errors, status: :unprocessable_entity 
     end
   end
 
-  # DELETE /organizations/1
-  # DELETE /organizations/1.json
+  # DELETE api/organizations/1
   def destroy
     @organization = Organization.find(params[:id])
     @organization.destroy
-
-    respond_to do |format|
-      format.html { redirect_to organizations_url }
-      format.json { head :no_content }
-    end
+    render json: @organization, status: :no_content
   end
 
 end
